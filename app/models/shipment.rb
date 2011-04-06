29bcf2e13b3d4f9f60e7d017d6d5282376712e9d
Sue Grimes <sgrimes@stanford.edu>
2011-04-05 17:01:38 -0700

    Modify shipment validation (require all checkboxes checked)

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -2,24 +2,40 @@
 #
 # Table name: shipments
 #
-#  id                :integer(4)      not null, primary key
-#  sample_id         :integer(4)      not null
-#  date_shipped      :date
-#  fedex_tracking_nr :string(50)
-#  date_received     :date
-#  comments          :string(255)
-#  updated_by        :integer(4)
-#  updated_at        :timestamp
+#  id                 :integer(4)      not null, primary key
+#  sample_id          :integer(4)      not null
+#  confirm_nr_cells   :string(1)
+#  confirm_barcode    :string(1)
+#  confirm_ship_day   :string(1)
+#  confirm_dry_ice    :string(1)
+#  confirm_fedex_nr   :string(1)
+#  confirm_ship_email :string(1)
+#  date_shipped       :date
+#  fedex_tracking_nr  :string(50)
+#  date_received      :date
+#  comments           :string(255)
+#  updated_by         :integer(4)
+#  updated_at         :timestamp
 #
 
 class Shipment < ActiveRecord::Base
-
   belongs_to :sample
   
   validates_presence_of :date_shipped, :fedex_tracking_nr
   
   SHIPMENT_DEFAULT = {:date_shipped => Date.today}
 
+  def validate
+    fld_not_checked = false
+    fld_not_checked = true if confirm_nr_cells != 'Y'
+    fld_not_checked = true if confirm_barcode != 'Y'
+    fld_not_checked = true if confirm_ship_day != 'Y'
+    fld_not_checked = true if confirm_dry_ice != 'Y'
+    fld_not_checked = true if confirm_fedex_nr != 'Y'
+    fld_not_checked = true if confirm_ship_email != 'Y'
+    errors.add_to_base("All shipping requirement checkboxes must be checked") if fld_not_checked
+  end
+  
   def ship_day(int_or_string='string')
     if int_or_string == 'int'
       return (date_shipped.nil? ? -1 : date_shipped.strftime("%w"))
@@ -31,4 +47,5 @@ class Shipment < ActiveRecord::Base
   def date_received_or_na
     (date_received.nil? ? 'N/A' : date_received)
   end
+  
 end

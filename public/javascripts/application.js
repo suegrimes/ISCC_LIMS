2a28b2d7aca67956deb865cc2fc61358529614c2
jpalm <jennifer.palm13@gmail.com>
2011-05-12 17:57:37 -0700

    added a basic numerical date function

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,13 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+function num_date() {
+    var mm = new Array("01", "02", "03","04", "05", "06", "07", "08", "09","10", "11", "12");
+    var full_date = new Date();
+    var curr_date = full_date.getDate();
+    var curr_month = full_date.getMonth();
+    var curr_year = full_date.getFullYear();
+    var num_date = curr_year + "-" + mm[curr_month] + "-" + curr_date;
+    return num_date;
+}
+

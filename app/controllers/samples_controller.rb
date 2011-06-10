fc30b9994904eea1b04285ddff57ae894a4b840f
jpalm <jennifer.palm13@gmail.com>
2011-06-09 19:53:45 -0700

    added mimetype and sample select options to form

--- app/controllers/samples_controller.rb
+++ app/controllers/samples_controller.rb
@@ -132,7 +132,15 @@ class SamplesController < ApplicationController
   end
   
   def list_sample_results
-    
+    # TODO
+    # Probably put this in sample results controller
+    # concatate Sample name with Barcode
+    # write to sample_results. allow to choose multiple samples
+    # get the list of linked samples from results_files from logged in's lab
+    # compare lists from file sys to this list. If linked already, display different status on form greyed or whatever
+    # figure out with Sue what angle to offer admins: link file to samples or link sample to files
+        
+    @samples = Sample.find(:all) 
     @sample = Sample.find(params[:id])
     
     # this for researchers and admins, to narrow down which files to get; only look in dir for the chosen lab 
@@ -140,19 +148,18 @@ class SamplesController < ApplicationController
     @user_lab_folder = @user_lab_folder.gsub!(/ /, '_') if @user_lab_folder.match(/\s/)
     
     @results = ResultFile.find(:all)
-    
-    # TODO: instead of this, loop through each file and also gather it's size, mimetype and other stuff
-    # push in to array of arrays or hash
-    
-    #@datafile_path = '../iscc_rnaseq/dataDownloads/'
-    #@datafile_path = 'public/files/dataDownloads/' + @user_lab_folder + '/'
+            
+    #@datafile_path = RAILS_ROOT + 'public/files/dataDownloads/' + @user_lab_folder + '/'
     @datafile_path = RAILS_ROOT + '/public/files/dataDownloads/' + @user_lab_folder + '/'    
     Dir.chdir(@datafile_path)
     
     @file_list = [];
     Dir.foreach('.') {
         |fn|
-        @file_list.push([fn, File.size(fn)]) if (fn[0].chr != '.')
+        extname = File.extname(fn)[1..-1]
+        mime_type = Mime::Type.lookup_by_extension(extname)
+        content_type = mime_type.to_s unless mime_type.nil?
+        @file_list.push([fn, File.size(fn), content_type]) if (fn[0].chr != '.')
     }
     
     #@list_files = Dir.glob("*")

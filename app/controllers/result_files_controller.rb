cdfdb6c3b90e0a8cfb344be975d9fc40560e1a24
jpalm <jennifer.palm13@gmail.com>
2011-06-28 17:35:23 -0700

    Work in progress: getting data in to the results file table and changes to view files

--- app/controllers/result_files_controller.rb
+++ app/controllers/result_files_controller.rb
@@ -6,49 +6,34 @@ class ResultFilesController < ApplicationController
   end
   
   def read_create_files
-
-    @user_lab_folder = current_user.lab.lab_name.downcase    
-    @user_lab_folder = @user_lab_folder.gsub!(/ /, '_') if @user_lab_folder.match(/\s/)
-     
-    @datafile_path = RAILS_ROOT + '/public/files/dataDownloads/' + @user_lab_folder + '/'    
-    Dir.chdir(@datafile_path)
     
-    @file_system_list = [];
-    @result_files_list = [];
-    file_info = {};
-    Dir.foreach('.') {
-        |fn|
-        extname = File.extname(fn)[1..-1]
-        mime_type = Mime::Type.lookup_by_extension(extname)
-        content_type = mime_type.to_s unless mime_type.nil?
-         
-          file_info = {
-            :lab_id => current_user.lab.id,
-            :document => fn,
-            :document_content_type => content_type,
-            :document_file_size => File.size(fn),
-            :updated_by => current_user.auth_user.name
-          }
+    #TODO
+    # get contents of result_files table
+    # check file is already in table - check_result_file(result_file)
+    # if so, add to a debug list. display in view
+    # if not, add to the @result_files_list, 
+    # add to an "added" list, display in view
 
-        @result_files_list.push(file_info) if (fn[0].chr != '.') 
-        @file_system_list.push([fn, File.size(fn), content_type, current_user.lab.id, current_user.auth_user.name]) if (fn[0].chr != '.')   
-    }
+    @result_files_list = get_files_from_filesystem
+    
+    files_saved = 0
+    @result_files_list.each do |file_info|
+      result_file = ResultFile.new(file_info)
+      if result_file.save
+        files_saved += 1           
+      end
+    end
+    
+    if files_saved != 0 
+      flash[:notice] = 'Success! ' + files_saved.to_s + ' new Result files saved'
+    else
+      flash[:error] = 'ERROR - Unable to save result files'
+    end
         
     Dir.chdir(RAILS_ROOT)
     
-    #TODO
-    # save to results_files table
-    # get contents of result_files table
-    # check file is already in table
-    # if so, add to a debug list. display in view
-    # if not, add to the @result_files_list, 
-    # add to an added list, display in view
-    # display contents of results_files in view
-    
-    render :partial => 'list_result_files', :locals => { :file_list => @result_files_list }
-    
   end
-  
+   
   def link_multi
     #authorize! :create, ResultFile
 
@@ -62,18 +47,8 @@ class ResultFilesController < ApplicationController
       # result_file.samples = Sample.find(params[:result_file][:sample_ids])
       # result_file.save      
     # also see her mail of 6/22
-    # !!! Ask Sue how to get a partial to show up and get it to use a function in the controller other than this one!!!
-
-    # function to check if the file is in table
-    #def check_result_file(result_file_param)
-      # check if file is already in the result_files table
-      #if result_file_param[:id].blank?
-        #return true
-      #else
-        #return nil
-    #end 
     
-    # the following code is temporary 
+    @files_from_filesystem = get_files_from_filesystem
 
     @samples = Sample.find(:all) 
     #@sample = Sample.find(params[:id])
@@ -82,24 +57,45 @@ class ResultFilesController < ApplicationController
     @user_lab_folder = current_user.lab.lab_name.downcase    
     @user_lab_folder = @user_lab_folder.gsub!(/ /, '_') if @user_lab_folder.match(/\s/)
     
-    @results = ResultFile.find(:all)
- 
+    @results = ResultFile.find(:all) 
+  end
+
+  def get_files_from_filesystem
+    @user_lab_folder = current_user.lab.lab_name.downcase    
+    @user_lab_folder = @user_lab_folder.gsub!(/ /, '_') if @user_lab_folder.match(/\s/)
+     
     @datafile_path = RAILS_ROOT + '/public/files/dataDownloads/' + @user_lab_folder + '/'    
     Dir.chdir(@datafile_path)
     
-    @file_system_list = [];
+    @files_list = [];
+    file_info = {};
     Dir.foreach('.') {
         |fn|
         extname = File.extname(fn)[1..-1]
         mime_type = Mime::Type.lookup_by_extension(extname)
         content_type = mime_type.to_s unless mime_type.nil?
-        @file_system_list.push([fn, File.size(fn), content_type, current_user.lab.id, current_user.auth_user.name]) if (fn[0].chr != '.')
+         
+        file_info = {
+          :lab_id => current_user.lab.id,
+          :document => fn,
+          :document_content_type => content_type,
+          :document_file_size => File.size(fn),
+          :updated_by => current_user.auth_user.name
+        }
+
+        @files_list.push(file_info) if (fn[0].chr != '.')                
     }
-    
-    #@list_files = Dir.glob("*")
-    
-    Dir.chdir(RAILS_ROOT)
+    return @files_list
   end
+  
+     # function to check if the file is in table
+    #def check_result_file(result_file)
+      # check if file is already in the result_files table
+      #if result_file_param[:id].blank?
+        #return true
+      #else
+        #return nil
+    #end 
   
   def debug
     render :action => :debug  

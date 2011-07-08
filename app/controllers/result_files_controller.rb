a6d2121d8f56c544bb60716002979a8402d6eee3
jpalm <jennifer.palm13@gmail.com>
2011-07-07 18:07:45 -0700

    Work in progress: narrowing down by lab only, and only saving files not alreay in db

--- app/controllers/result_files_controller.rb
+++ app/controllers/result_files_controller.rb
@@ -10,10 +10,9 @@ class ResultFilesController < ApplicationController
     #ResultFile.connection.execute("TRUNCATE TABLE result_files")
     
   # TODO
-  # query result_files table
-  # check if file is already in result_files table
-  ## if not in results_files table, make list and display as recent files added
-  ## .save
+  
+  # query result_files for chosen lab only
+  # in the save loop, put condition for if no files in documents variable
  
   # query linked table
   # query results_files and if file is in linked table
@@ -22,35 +21,46 @@ class ResultFilesController < ApplicationController
   ## if not display as unchecked
   ## else display as checked 
   
-  ## validation for No Lab Selected
+  # validation for No Lab Selected
+  # localize variables
+  # take out unneeded variables
     
     # temporary until user edit features are in place
-    ResultFile.connection.execute("TRUNCATE TABLE result_files")
-
+    #ResultFile.connection.execute("TRUNCATE TABLE result_files")
+    
     @lab = Lab.find(params[:lab][:id])    
     lab_dir_name = @lab.lab_name.downcase
     lab_dir_name = lab_dir_name.gsub!(/ /, '_') if lab_dir_name.match(/\s/) 
+    
+    @chosen_lab_id = @lab.id
        
-    @result_files_list = get_files_from_filesystem(lab_dir_name)
-    
+    @results_on_filesystem = get_files_from_filesystem(lab_dir_name, @chosen_lab_id)
+    @result_files_before_save = ResultFile.find(:all, :conditions => {:lab_id => @chosen_lab_id})
+    documents = @result_files_before_save.collect(&:document) if (!@result_files_before_save.blank?)
+
+    @exist_in_db = []; @new_files = [];
     files_saved = 0
-    @result_files_list.each do |file_info|
-      result_file = ResultFile.new(file_info)
-      if result_file.save
-        files_saved += 1           
-      end
-    end
-    
+    @results_on_filesystem.each do |file_info|   
+      # check if file is already in the table
+      unless (documents.include?(file_info[:document]))
+        @new_files.push(file_info[:document])
+        result_file = ResultFile.new(file_info)
+        if result_file.save
+          files_saved += 1           
+        end # if
+      end # unless
+    end # each
+
     if files_saved != 0 
       flash[:notice] = 'Success! ' + files_saved.to_s + ' new Result files saved'
     else
-      flash[:error] = 'ERROR - Unable to save result files'
+      flash[:error] = 'No new result files to save'
     end
         
     Dir.chdir(RAILS_ROOT)
     
     @labs = Lab.find(:all, :order => :lab_name)
-    @result_files = ResultFile.find(:all)
+    @result_files = ResultFile.find(:all, :conditions => {:lab_id => @chosen_lab_id})
     @samples = Sample.find(:all)     
     
     render :action => 'link_multi'
@@ -61,7 +71,7 @@ class ResultFilesController < ApplicationController
     #authorize! :create, ResultFile
 
     # TODO
-    # display all files with the lab.id in form
+    # display all files with the lab.name in form
     # checked or not depending on if already linked
     # user can: link samples, update, delete(?)
     # some handy code examples from Sue:
@@ -70,6 +80,8 @@ class ResultFilesController < ApplicationController
       # result_file.samples = Sample.find(params[:result_file][:sample_ids])
       # result_file.save      
     # also see her mail of 6/22
+    # localize variables
+    # take out unneeded variables
  
     @labs = Lab.find(:all, :order => :lab_name)
     @result_files = ResultFile.find(:all)
@@ -77,7 +89,7 @@ class ResultFilesController < ApplicationController
        
   end
 
-  def get_files_from_filesystem(dir_name)
+  def get_files_from_filesystem(dir_name, lab_id)
     
     # TODO - change current user lab to chosen lab
      
@@ -93,7 +105,7 @@ class ResultFilesController < ApplicationController
         content_type = mime_type.to_s unless mime_type.nil?
          
         file_info = {
-          :lab_id => current_user.lab.id,
+          :lab_id => lab_id,
           :document => fn,
           :document_content_type => content_type,
           :document_file_size => File.size(fn),

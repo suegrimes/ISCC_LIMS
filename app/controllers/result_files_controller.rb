f42b429425afc3d495f4385979d8205e575c860c
jpalm <jennifer.palm13@gmail.com>
2011-07-14 19:15:10 -0700

    Work in progress: lab form working from both index and link_multi conditions for no or empty lab dir new files pretty view

--- app/controllers/result_files_controller.rb
+++ app/controllers/result_files_controller.rb
@@ -21,66 +21,68 @@ class ResultFilesController < ApplicationController
     #ResultFile.connection.execute("TRUNCATE TABLE result_files")
     
   # TODO
-  # add debug for new files
-  # in the save loop, put condition for if no files in documents variable?
- 
-  # SAVE TO LINKED TABLE
+  # make flash message disappear from subsequent pages
+  # dump table and see what happens
 
+  # SAVE TO LINKED TABLE
   # query linked table
   # query results_files
   # if result_file already in linked table:
   ## display as checked 
   ## else display as unchecked
-    
+
+  # query result_files for updated_by (an id#), then query auth_user table by id# to get list of names
+  # make a hash or ? to connect the file with this name rather than their id# for display in the link form
+
   # localize variables
   # take out unneeded variables
+
   # some handy code examples from Sue:
       # result_file = ResultFile.find(params[:result_file][:id])
       # result_file.update_attributes(params[:result_file])
       # result_file.samples = Sample.find(params[:result_file][:sample_ids])
       # result_file.save      
   # also see her mail of 6/22
+ 
+  # add update, edit 
     
     # temporary until user edit features are in place
     #ResultFile.connection.execute("TRUNCATE TABLE result_files")
-    
+
+    @labs = Lab.find(:all, :order => :lab_name) 
     @lab = Lab.find_by_id(params[:lab_id])
     lab_dir_name = @lab.lab_name.downcase
     lab_dir_name = lab_dir_name.gsub!(/ /, '_') if lab_dir_name.match(/\s/) 
        
     @results_on_filesystem = get_files_from_filesystem(lab_dir_name, @lab.id)
+    if (@results_on_filesystem.blank?) #directory does not exist or is empty
+      flash[:error] = "Sorry, no result files available for #{@lab.lab_name}"
+      render :action => 'index', :locals => {:lab_list => @labs}
+      return
+    end
     
     @result_files_before_save = ResultFile.find(:all, :conditions => {:lab_id => @lab.id})
-    @documents = @result_files_before_save.collect(&:document) if (!@result_files_before_save.blank?)
+    documents_in_db = @result_files_before_save.collect(&:document) if (!@result_files_before_save.blank?)
 
-=begin
     @exist_in_db = []; @new_files = [];
-    files_saved = 0
-    @results_on_filesystem.each do |file_info|   
+    @files_saved = 0
+    @results_on_filesystem.each do |file_on_system_info|   
       # check if file is already in the table
-      unless (documents.include?(file_info[:document]))
-        @new_files.push(file_info[:document])
-        result_file = ResultFile.new(file_info)
+      if (documents_in_db.blank?) || (!documents_in_db.include?(file_on_system_info[:document]))
+        @new_files.push(file_on_system_info[:document])
+        result_file = ResultFile.new(file_on_system_info)
         if result_file.save
-          files_saved += 1           
+          @files_saved += 1           
         end # if
-      end # unless
+      else
+        @exist_in_db.push(file_on_system_info[:document])
+      end # if
     end # each
-
-    if files_saved != 0 
-      flash[:notice] = 'Success! ' + files_saved.to_s + ' new Result files saved'
-    else
-      flash[:error] = 'No new result files to save'
-    end
-        
-    
-    
-    @labs = Lab.find(:all, :order => :lab_name)
-    @result_files = ResultFile.find(:all, :conditions => {:lab_id => @chosen_lab_id})
-    @samples = Sample.find(:all)     
-=end    
-    #render :action => 'link_multi'
-    
+            
+    # get data for link form        
+    @result_files = ResultFile.find(:all, :conditions => {:lab_id => @lab.id})
+    @samples = Sample.find(:all)   
+   
   end
     
   def debug
@@ -93,12 +95,15 @@ protected
     # TODO
     # localize variables
     
-    @datafile_path = RAILS_ROOT + '/public/files/dataDownloads/' + dir_name + '/'    
-    Dir.chdir(@datafile_path)
+    # check if directory exists
+    datafile_path = RAILS_ROOT + '/public/files/dataDownloads/' + dir_name + '/' 
+    if (File.directory?(datafile_path))
+           
+      Dir.chdir(datafile_path)
     
-    @files_list = [];
-    file_info = {};
-    Dir.foreach('.') {
+      files_list = [];
+      file_info = {};
+      Dir.foreach('.') {
         |fn|
         extname = File.extname(fn)[1..-1]
         mime_type = Mime::Type.lookup_by_extension(extname)
@@ -109,15 +114,16 @@ protected
           :document => fn,
           :document_content_type => content_type,
           :document_file_size => File.size(fn),
-          :updated_by => current_user.auth_user.name
+          :updated_by => current_user.auth_user.id
         }
-
-        @files_list.push(file_info) if (fn[0].chr != '.')                
-    }
-    
-    Dir.chdir(RAILS_ROOT)
-    return @files_list
-    
+        files_list.push(file_info) if (fn[0].chr != '.')                
+      }
+      
+      Dir.chdir(RAILS_ROOT)    
+      return files_list   
+    else    
+      return nil    
+    end
   end
   
 end

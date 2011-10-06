5718f6636ac7fcfe79edc430679c4ff275501f35
Sue Grimes <sgrimes@stanford.edu>
2011-10-06 15:08:27 -0700

    Updates to result file dirs/views

--- app/controllers/result_files_controller.rb
+++ app/controllers/result_files_controller.rb
@@ -1,53 +1,46 @@
 class ResultFilesController < ApplicationController
   load_and_authorize_resource
   
-  #DATAFILES = '/relative path on production server/' 
-  DATAFILES = '../../test/dataDownloads/' #dev
-  
   def index
     @result_files = ResultFile.find(:all, :include => {:seq_lanes => :sample}, :conditions => {:lab_id => current_user.lab.id})
-    labname_dir = get_lab_dirname(current_user.lab.id)
-    @fastqc_files = get_fastqc_files(labname_dir)
+    @fastqc_files = get_fastqc_html(Lab.find(current_user.lab.id).lab_dirname)
   end
   
   def fastqc_show    
     @fastqc_file = params[:file_path]
-    headers["Content-Type"] = "zip"
-    send_file(DATAFILES + @fastqc_file)
+    send_file(@fastqc_file, :type => 'html', :disposition => 'inline')
   end
     
   def show
     rfile = ResultFile.find(params[:id]) 
-    labname_dir = get_lab_dirname(current_user.lab.id)   
-    @datafile_path = DATAFILES + labname_dir + '/'
-    send_file(File.join(@datafile_path, rfile[:document]), :type => rfile[:document_content_type], :disposition => 'inline')
+    labname_dir = Lab.find(current_user.lab.id).lab_dirname   
+    send_file(File.join(ResultFile::BASE_PATH, labname_dir, rfile[:document]), 
+                        :type => rfile[:document_content_type], :disposition => 'inline')
   end
   
   def choose_lab
     @labs = Lab.find(:all, :order => :lab_name)    
   end
   
-  def check_chosen_lab
-    @labs = Lab.find(:all, :order => :lab_name)
-    @chosen_lab_id = params[:lab][:id] if (!params[:lab][:id].blank?)
-    if (params[:lab][:id].blank?)
-       flash.now[:error] = 'Choose Lab with Result Files'
-       render :action => 'choose_lab'
+  def check_chosen_lab       
+    if (params[:lab] && !params[:lab][:id].blank?)
+      redirect_to :action => 'edit_multi', :lab_id => params[:lab][:id]
     else
-      redirect_to :action => 'edit_multi', :lab_id => @chosen_lab_id
+      flash.now[:error] = 'Choose Lab with Result Files'
+      @labs = Lab.find(:all, :order => :lab_name)
+      render :action => 'choose_lab'
     end
   end
   
   def edit_multi 
-
     @labs = Lab.find(:all, :order => :lab_name) 
-    @chosen_lab  = Lab.find_by_id(params[:lab_id])
-    chosen_lab_dir_id   = @chosen_lab.id
-    chosen_lab_dir_name = get_lab_dirname(chosen_lab_dir_id)
+    @chosen_lab  = Lab.find(params[:lab_id])
+    @datafile_path = File.join(ResultFile::BASE_PATH, @chosen_lab.lab_dirname)
        
-    @results_on_filesystem = get_files_from_filesystem(chosen_lab_dir_name, chosen_lab_dir_id)
+    @results_on_filesystem = get_files_from_filesystem(@chosen_lab.lab_dirname, @chosen_lab.id)
     if (@results_on_filesystem.blank?) #directory does not exist or is empty
-      flash.now[:error] = "Sorry, no result files available for #{@chosen_lab.lab_name}"
+      #flash.now[:error] = "Sorry, no result files available for #{@chosen_lab.lab_name}"
+      flash.now[:error] = "Sorry, no result files found for #{@chosen_lab.lab_name} in directory: #{@datafile_path}"
       render :action => 'choose_lab', :locals => {:lab_list => @labs}
       return
     end
@@ -101,24 +94,21 @@ class ResultFilesController < ApplicationController
       flash.now[:notice] = @files_updated.to_s + " updates were made to Result Files table"
     end
     
-    @chosen_lab = Lab.find_by_id(params[:chosen_lab][:id])
+    @chosen_lab = Lab.find(params[:chosen_lab][:id])
     #get result files with associated lanes and samples per lab chosen by admin
     @result_files = ResultFile.find(:all, :include => {:seq_lanes => :sample}, :conditions => {:lab_id => params[:chosen_lab][:id]})
     render :action => 'update_multi_show'    
   end
   
-  def destroy
- 
-    lab_dir_name = get_lab_dirname(params[:lab_id]) 
-    
+  def destroy     
     result_file = ResultFile.find(params[:id])
     authorize! :delete, ResultFile
     
     result_file.destroy
-    File.delete(DATAFILES + lab_dir_name + '/' + result_file.document)      
+    lab_dir_name = Lab.find(params[:lab_id]).lab_dirname  
+    File.delete(File.join(ResultFile::BASE_PATH, lab_dir_name, result_file.document))     
     
-    redirect_to :action => 'edit_multi', :lab_id => params[:lab_id]
-    
+    redirect_to :action => 'edit_multi', :lab_id => params[:lab_id]   
   end
   
   def debug
@@ -128,71 +118,58 @@ class ResultFilesController < ApplicationController
 protected
   
   def get_files_from_filesystem(lab_dir_name, lab_id)
-    # TODO
-    # localize variables
+    datafile_path = File.join(ResultFile::BASE_PATH, lab_dir_name) 
+    files_list = []
     
-    # check if directory exists
-    datafile_path = DATAFILES + lab_dir_name + '/' 
     if (File.directory?(datafile_path))
-           
-      Dir.chdir(datafile_path)
+      fn_list = get_file_list(datafile_path)
+      fn_list.each do |fn|
+        files_list.push({:lab_id => lab_id,
+                         :document => fn,
+                         :document_content_type => file_content_type(fn),
+                         :document_file_size => File.size(File.join(datafile_path,fn)),
+                         :updated_by => current_user.id})
+      end
+    end  
+    return files_list
+  end
+  
+  def get_fastqc_html(lab_dir_name)
+    datafile_path = File.join(RAILS_ROOT, ResultFile::BASE_PATH, lab_dir_name)
+    dirs_list = get_dir_list(datafile_path, '_fastqc')
     
-      files_list = [];
-      file_info = {};
-      Dir.foreach('.') {        
-        |fn|
-        extname = File.extname(fn)[1..-1]
-        mime_type = Mime::Type.lookup_by_extension(extname)
-        content_type = mime_type.to_s unless mime_type.nil?
-        next if ((File.directory?(fn)) || (fn[0].chr == '.')) || (mime_type == 'zip')
-         
-        file_info = {
-          :lab_id => lab_id,
-          :document => fn,
-          :document_content_type => content_type,
-          :document_file_size => File.size(fn),
-          :updated_by => current_user.auth_user.id
-        }
-        files_list.push(file_info)             
-      }
-      
-      Dir.chdir(RAILS_ROOT)    
-      return files_list   
-    else    
-      return nil    
+    files_list = []
+    dirs_list.each do |fdir|
+      next if (fdir[0].chr == '.')
+      qcdir_path = File.join(datafile_path, fdir)
+      files_list.push(File.join(qcdir_path, get_file_list(qcdir_path, 'html')))
     end
+
+    return files_list.flatten 
   end
   
-  def get_fastqc_files(lab_dir_name)
-    
-    datafile_path = DATAFILES + lab_dir_name + '/' 
-    
-    if (File.directory?(datafile_path))
-           
-      Dir.chdir(datafile_path)
-    
-      files_list = []
-      Dir.foreach('.') { # go through dir looking for fastqc directories       
-        |fn|
-        next if ((File.directory?(fn)) || (fn[0].chr == '.'))
-        fastqc_dir = fn
-        files_list.push(lab_dir_name + '/' + fastqc_dir) if fn.match('fastqc.zip')
-      } 
-      
-      Dir.chdir(RAILS_ROOT) 
-      
-      return files_list
-    else    
-      return nil    
-    end # if datafile path  
-  end
-  
-  def get_lab_dirname(lab_id)
-    lab  = Lab.find_by_id(lab_id)
-    lab_dir_name = lab.lab_name.downcase
-    lab_dir_name = lab_dir_name.gsub!(/ /, '_') if lab_dir_name.match(/\s/)
-    return lab_dir_name
-  end
+#  def get_fastqc_files(lab_dir_name)
+#    
+#    datafile_path = File.join(ResultFile::BASE_PATH, lab_dir_name) 
+#    
+#    if (File.directory?(datafile_path))
+#           
+#      Dir.chdir(datafile_path)
+#    
+#      files_list = []
+#      Dir.foreach('.') do |fdir| # go through dir looking for fastqc zip files       
+#        next if ((File.directory?(fn)) || (fn[0].chr == '.'))
+#        fastqc_dir = fn
+#        files_list.push(lab_dir_name + '/' + fastqc_dir) if fn.match('fastqc.zip')
+#      end
+#         
+#      Dir.chdir(RAILS_ROOT) 
+#      
+#      return files_list
+#    else    
+#      return nil    
+#    end # if datafile path  
+#  end
   
   # debug for development only
   def trunc_tables
